24f117ae9860919e3616541ca91d600bee420ead
Eliezer Mas y rubi <elimasyrubi@gmail.com>
2020-03-14 19:28:45 -0300

    Navbar finished

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./styles/Navbar.scss"
 import Logo from '../images/marvelLogo.png'
+import SearchIcon from '../images/search.svg'
+import StarIcon from '../images/star.svg'
 
 class Navbar extends React.Component{
     render(){
@@ -9,7 +11,9 @@ class Navbar extends React.Component{
                 <img className="Navbar_logo" src={Logo} alt="logo marvel"/>
                 <hr className="Navbar_line"/>
                 <form>
+                    <button><img src={SearchIcon} className="Navbar_icon" alt="SearchIcon"/></button>
                     <input type="text" placeholder="Buscar" className="Navbar_input"/>
+                    <button><img src={StarIcon} className="Navbar_icon" alt="StarIcon"/></button>
                 </form>
                 <hr className="Navbar_line"/>
             </div>
