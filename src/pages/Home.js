c8786b6fb32077ca8d38f5c6661c5b351aee7bc3
Eliezer Mas y rubi <elimasyrubi@gmail.com>
2020-03-16 01:39:19 -0300

    cards finished

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import "./styles/Home.scss";
 import Card from "../components/Card";
 import Modal from "../components/Modal";
 import SearchResult from "../components/SearchResult";
-import StarIcon from "../images/star.svg";
+
 
 class Home extends React.Component {
   state = {
@@ -25,48 +25,17 @@ class Home extends React.Component {
     );
     const data = await response.json();
     this.setState({ comicData: data });
-    console.log(this.state.comicData.data.results[0].name);
+    
   };
 
   render() {
     return (
       <div className="background">
-        <div className="card_container container">
-          {/* <Card /> */}
-          <ul className="card">
-            {this.state.comicData.data.results.map(character => (
-              <li key={character.id}>
-                <img
-                  className="card_image"
-                  src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-                  alt="Super Heroe"
-                />
-                <a className="card_icon" href="#">
-                  <img src={StarIcon} className="card_icon" alt="StarIcon" />
-                </a>
-            <p className="card_name">{character.name}</p>
-              </li>
-            ))}
-          </ul>
+        <div>
+          <Card data={this.state.comicData.data.results} /> 
         </div>
 
-        {/*     <ul>
-            {this.state.comicData.data.results.map(character => (
-              <li key={character.id}>
-                <p>{character.name}</p>
-              </li>
-            ))}
-          </ul>
 
-          <div className="card">
-            <img className="card_image" src={SuperHeroe} alt="Super Heroe" />
-            <a className="card_icon" href="#">
-              <img src={StarIcon} className="card_icon" alt="StarIcon" />
-            </a>
-            <p className="card_name">3d man</p>
-          </div>
-        </div>
- */}
         <Modal isOpen={false}>
           <h2 className="modal_tittle">Spiderman</h2>
           <SearchResult />
