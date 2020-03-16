8b563bbdbbc37c36b0c922cc164729904c98d5dd
Eliezer Mas y rubi <elimasyrubi@gmail.com>
2020-03-16 02:30:30 -0300

    Css mejorado y comienzo de LoadingPage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import "./styles/Home.scss";
 import Card from "../components/Card";
+import LoadingPage from "../components/LoadingPage";
 import Modal from "../components/Modal";
 import SearchResult from "../components/SearchResult";
 
 
 class Home extends React.Component {
   state = {
+    loading:true,
     comicData: {
       data: {
         results: []
@@ -24,11 +26,14 @@ class Home extends React.Component {
       "https://gateway.marvel.com:443/v1/public/characters?apikey=b880453eb1ab8bd6dab05e2d968864ad"
     );
     const data = await response.json();
-    this.setState({ comicData: data });
+    this.setState({ comicData: data, loading: false });
     
   };
 
   render() {
+    if(this.state.loading){
+      return <LoadingPage/>
+    }
     return (
       <div className="background">
         <div>
